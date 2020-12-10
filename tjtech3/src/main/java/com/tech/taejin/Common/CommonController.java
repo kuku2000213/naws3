dcb242b7f1c4475ededed232a2a8c100bce833f6
kuku2000213 <nd10@narangdesign.com>
2020-12-10 10:00:46 +0900

    commit2

---
+++ tjtech3/src/main/java/com/tech/taejin/Common/CommonController.java
@@ -0,0 +1,14 @@
+package com.tech.taejin.Common;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CommonController {
+
+	@RequestMapping("/")
+	public String home() {
+		
+		return "common/home";
+	}
+}
